3cd801636ce4f94fb6ea475a2f64b5ae478a7764
jesse <z2070461@students.niu.edu>
2026-03-02 16:41:19 -0600

    initial commit

---
+++ resources/views/components/⚡birth.blade.php
@@ -0,0 +1,31 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    public function show($id) {
+
+    }
+
+    public function store() {
+
+    }
+    
+    public function update($id) {
+
+    }
+    
+    public function destroy($id) {
+
+    }
+    
+    public function export() {
+        
+    }
+};
+?>
+
+<div>
+    {{-- Simplicity is the essence of happiness. - Cedric Bledsoe --}}
+</div>
